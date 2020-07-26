57ec9257048d08c0e853b3fb880253df8b525682
tanmaypardeshi <tanmaypardeshi@gmail.com>
2020-07-27 00:52:59 +0530

    Added color

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -4,6 +4,8 @@ import { Bar } from 'react-chartjs-2';
 import { Typography, CircularProgress } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 
+const color = ['rgba(255, 99, 132, 0.8)','rgba(54, 162, 235, 0.8)','rgba(255, 206, 86, 0.8)','rgba(75, 192, 192, 0.8)','rgba(153, 102, 255, 0.8)','rgba(255, 159, 64, 0.8)','rgba(255, 99, 132, 0.8)','rgba(54, 162, 235, 0.8)','rgba(255, 206, 86, 0.8)','rgba(75, 192, 192, 0.8)','rgba(153, 102, 255, 0.8)','rgba(255, 159, 64, 0.8)','rgba(255, 99, 132, 0.8)','rgba(54, 162, 235, 0.8)','rgba(255, 206, 86, 0.8)','rgba(75, 192, 192, 0.8)','rgba(153, 102, 255, 0.8)','rgba(255, 159, 64, 0.8)','rgba(255, 99, 132, 0.8)','rgba(54, 162, 235, 0.8)','rgba(255, 206, 86, 0.8)','rgba(75, 192, 192, 0.8)','rgba(153, 102, 255, 0.8)','rgba(255, 159, 64, 0.8)','rgba(255, 99, 132, 0.8)','rgba(54, 162, 235, 0.8)','rgba(255, 206, 86, 0.8)','rgba(75, 192, 192, 0.8)','rgba(153, 102, 255, 0.8)','rgba(255, 159, 64, 0.8)','rgba(255, 99, 132, 0.8)','rgba(54, 162, 235, 0.8)','rgba(255, 206, 86, 0.8)','rgba(255, 99, 132, 0.8)','rgba(54, 162, 235, 0.8)','rgba(255, 206, 86, 0.8)','rgba(75, 192, 192, 0.8)',]
+
 const useStyles = makeStyles({
     loading : {
         margin: 'auto',
@@ -53,10 +55,26 @@ const Charts = () => {
                                 {
                                     label:'Population',
                                     data: labels.map( label => label.totalConfirmed),
-                                    backgroundColor: 'green'
+                                    backgroundColor: color,
+                                    borderColor: color,
+                                    borderWidth: 2,
+                                    hoverBorderWidth:2,
+                                    hoverBorderColor:'#000'
+
                                 }
-                            ]
-                            }}
+                            ],
+                        }}  
+                        options={{
+                            title:{
+                                display:true,
+                                text: 'Statewise graph of COVID-19 confirmed cases',
+                                fontSize:26
+                            },
+                            legend:{
+                                display:true,
+                                position:'right'
+                            }
+                        }}
                         width={100}
                         height={50}
                     />
