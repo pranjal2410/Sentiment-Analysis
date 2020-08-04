3d114d2893e9bb08501a51086049c702fe8b4721
tanmaypardeshi <tanmaypardeshi@gmail.com>
2020-08-05 01:05:26 +0530

    Basic Auth added

--- backend/app/admin.py
+++ backend/app/admin.py
@@ -1,33 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from . models import *
-from . forms import *
+from .models import User
 
-
-class UserAdmin(BaseUserAdmin):
-    form = UserAdminChangeForm
-    add_form = UserAdminCreationForm
-
-    list_display = ('email', 'fname', 'lname', 'is_superuser', 'twitter')
-    list_filter = ('is_superuser', 'twitter')
-    fieldsets = (
-        ('User Credentials', {'fields' : ('email', 'password')}),
-        ('Personal Details', {'fields' : ('fname', 'lname', 'twitter')}),
-        ('Location Details', {'fields' : ('city', 'state')}),
-        ('Rights', {'fields': ('is_staff', 'is_superuser')})
-    )
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide', ),
-            'fields': ('email', 'password1', 'password2', )
-        })
-    )
-    search_fields = ('email', 'fname', 'lname', 'city', 'state')
-    ordering = ('email', 'fname', 'lname', 'city', 'state')
-    filter_horizontal = ()
-
-
-admin.site.site_header = 'Sentiment Analysis'
-admin.site.register(User, UserAdmin)
+admin.site.register(User)
 admin.site.unregister(Group)
--- backend/app/models.py
+++ backend/app/models.py
@@ -1,55 +1,19 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractUser
+
+from .managers import UserManager
 
 
-class UserManager(BaseUserManager):
-    def create_user(self, email, fname, lname, state, city, twitter, password=None):
-        if not email:
-            raise ValueError("Please enter email")
-
-        user = self.model(
-            email=self.normalize_email(email),
-            fname=fname,
-            lname=lname,
-            city=city,
-            state=state,
-            twitter=twitter
-        )
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, password):
-        user = self.model(email=self.normalize_email(email))
-        user.set_password(password)
-        user.is_staff = True
-        user.is_superuser = True
-        user.save(using=self._db)
-        return user
-
-
-class User(AbstractUser, PermissionsMixin):
+class User(AbstractUser):
+    username = None
     email = models.EmailField(max_length=120, null=False, unique=True)
-    fname = models.CharField(max_length=30, null=False, default="admin")
-    lname = models.CharField(max_length=30, null=False, default="user")
-    city = models.CharField(max_length=30, null=False, default="city")
-    state = models.CharField(max_length=30, null=False, default="state")
-    twitter = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
-
-    objects = UserManager()
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
-    def get_full_name(self):
-        return self.fname + " " + self.lname
-
-    def get_short_name(self):
-        return self.fname
+    objects = UserManager()
 
     def __str__(self):
         return self.email
-
